5c756e487d0f55024b8b24970fe7b3c64630fbc3
imaboss90 <54406879+imaboss90@users.noreply.github.com>
2019-09-10 13:00:50 -0500

    finished

--- app/src/main/java/com/example/madlib/InfoActivity.java
+++ app/src/main/java/com/example/madlib/InfoActivity.java
@@ -34,30 +34,26 @@ public class InfoActivity extends AppCompatActivity {
 
         if(story.equalsIgnoreCase("Story 1")) {
             String strToDisplay = "My name is " + myName + " and I am the greatest immortal being to ever"
-                    + " walk this earth!" + " I am of the " + myPantheon + " pantheon, one that has been " +
-                    "ruling throughout time itself. " + "When I went to go meet Zeus the other day, he wanted to give me" +
+                    + " walk this earth!" + " On the weekends you can catch me eating some " + myPantheon + ", which I find very delicious."
+                    + "When I went to go meet Zeus the other day, he wanted to give me" +
                     " a " + myAdj + " " + myNoun + ". It was by far my favorite gift I have ever received and wish to" +
-                    " give it to all." + story;
+                    " give it to all.";
             TextView str = (TextView) findViewById(R.id.info);
             str.setText(strToDisplay);
         }
 
         if(story.equalsIgnoreCase("Story 2")) {
-            String strToDisplay = "My name is " + myName + " and I am the greatest immortal being to ever"
-                    + " walk this pluto!" + " I am of the " + myPantheon + " pantheon, one that has been " +
-                    "ruling throughout time itself. " + "When I went to go meet Zeus the other day, he wanted to give me" +
-                    " a " + myAdj + " " + myNoun + ". It was by far my favorite gift I have ever received and wish to" +
-                    " give it to all." + story;
+            String strToDisplay = "It was a very " + myAdj + " night in October. " + myName + " was trying to find their "
+                    + myNoun + " that got lost in the forest. Maybe it could be persuaded to come back?" + myName + " took the "
+                    + myPantheon + " and cooked it in a pot. The smell of the " + myPantheon + " was so good, it brought back their " + myNoun + ".";
             TextView str = (TextView) findViewById(R.id.info);
             str.setText(strToDisplay);
         }
 
         if(story.equalsIgnoreCase("Story 3")) {
-            String strToDisplay = "My name is " + myName + " and I am the greatest immortal being to ever"
-                    + " walk this mars!" + " I am of the " + myPantheon + " pantheon, one that has been " +
-                    "ruling throughout time itself. " + "When I went to go meet Zeus the other day, he wanted to give me" +
-                    " a " + myAdj + " " + myNoun + ". It was by far my favorite gift I have ever received and wish to" +
-                    " give it to all." + story;
+            String strToDisplay = "Over the years I've been contemplating if I want to open up a restaraunt where I will sell "
+                    + myPantheon + ". My friend " + myName + " said it was a rookie move and would turn me into a " + myAdj + " "
+                    + myNoun + ". So I don't think I'm going through with that idea...";
             TextView str = (TextView) findViewById(R.id.info);
             str.setText(strToDisplay);
         }
